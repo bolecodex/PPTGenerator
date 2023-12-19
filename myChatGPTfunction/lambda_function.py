008af84f08a1c310d11ad17b99b885103dca3e4d
Weibo Zhao <bolecodex@gmail.com>
2023-12-19 15:52:47 +0100

    add demo lambda function

---
+++ myChatGPTfunction/lambda_function.py
@@ -0,0 +1,24 @@
+import openai
+import os
+def lambda_handler(event, context):
+	openai.api_key = os.getenv('API_KEY')
+	location = "New York, NY"
+	messages =[
+	{"role":"system", "content": "You are a helpful assistant."},
+	{"role":"user", "content": f"Tell me an interesting fact about flocation3 in less than 200 characters"}
+	]
+
+	chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+	gpt_response = chat.choices[0].message.content
+	html_output = f"""
+	<h1>Property for sale in flocation]</h1>
+	<p>igpt_response]<p>
+	"""
+	print("gpt_response:", gpt_response)
+	return {
+		"statusCode": 200,
+		"body": html_output,
+		"headers": {
+			"Content-Type": 'text/html',
+			}
+		}
