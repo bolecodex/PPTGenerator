6c34dc1a923ea0c3ffe36344ab2304ac5674c9cf
Weibo Zhao <bolecodex@gmail.com>
2023-12-20 23:31:30 +0100

    fix typo

--- myChatGPTfunction/lambda_function.py
+++ myChatGPTfunction/lambda_function.py
@@ -5,14 +5,14 @@ def lambda_handler(event, context):
 	location = "New York, NY"
 	messages =[
 	{"role":"system", "content": "You are a helpful assistant."},
-	{"role":"user", "content": f"Tell me an interesting fact about flocation3 in less than 200 characters"}
+	{"role":"user", "content": f"Tell me an interesting fact about {location} in less than 200 characters"}
 	]
 
 	chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
 	gpt_response = chat.choices[0].message.content
 	html_output = f"""
-	<h1>Property for sale in flocation]</h1>
-	<p>igpt_response]<p>
+	<h1>Property for sale in {location}</h1>
+	<p>{gpt_response}<p>
 	"""
 	print("gpt_response:", gpt_response)
 	return {
